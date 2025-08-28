ab82ac692129f0e1e5ef6e3ccc17143fa1f0d60b
Prince Kumar <peincekumar8271@gmail.com>
2025-08-28 23:48:18 +0530

    Error page configer

---
+++ Web/Controllers/Error/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer;
+using BusinessLayer.Interface;
+using Common;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Error404()
+        {
+            return View("Error");
+        }
+    }
+}
